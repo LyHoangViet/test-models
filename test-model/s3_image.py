5e60cdee00be4b04b16b533c69e01079892c6090
LyHoangViet <hviet519@gmail.com>
2025-05-09 17:50:27 +0700

    add new models and update code

---
+++ test-model/s3_image.py
@@ -0,0 +1,57 @@
+import base64
+import boto3
+import json
+# Create a Bedrock Runtime client in the AWS Region of your choice.
+client = boto3.client(
+    "bedrock-runtime",
+    region_name="us-east-1",
+)
+
+MODEL_ID = "us.amazon.nova-premier-v1:0"
+# Define your system prompt(s).
+system_list = [
+    {
+        "text": "Bạn là một chuyên gia phân tích"
+    }
+]
+# Define a "user" message including both the image and a text prompt.
+message_list = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "image": {
+                    "format": "jpg",
+                    "source": {
+                        "s3Location": {
+                            "uri": "s3://test-content-vinamilk/image/vinamilk-1.jpg", 
+                            "bucketOwner": "637423316258"
+                        }
+                    }
+                }
+            },
+            {
+                "text": "Provide titles for this image."
+            }
+        ]
+    }
+]
+# Configure the inference parameters.
+inf_params = {"maxTokens": 300, "topP": 0.1, "topK": 20, "temperature": 0.3}
+
+native_request = {
+    "schemaVersion": "messages-v1",
+    "messages": message_list,
+    "system": system_list,
+    "inferenceConfig": inf_params,
+}
+# Invoke the model and extract the response body.
+response = client.invoke_model(modelId=MODEL_ID, body=json.dumps(native_request))
+model_response = json.loads(response["body"].read())
+# Pretty print the response JSON.
+print("[Full Response]")
+print(json.dumps(model_response, indent=2))
+# Print the text content for easy readability.
+content_text = model_response["output"]["message"]["content"][0]["text"]
+print("\n[Response Content Text]")
+print(content_text)
